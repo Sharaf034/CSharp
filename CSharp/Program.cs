6e55504072d3a1cd73ae0bec2240954c64d0a2d2
Sharaf034 <sharafet@code.edu.az>
2023-07-13 00:45:31 +0400

    add csharp file

---
+++ CSharp/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+//n ve m ededleri cutturse, cemini tap
+
+//int n = 12;
+//int m = 4;
+//int sum;
+
+//if (n % 2 == 0 && m % 2 == 0) ;
+
+//{
+//    sum = n + m;
+//    Console.WriteLine(sum); 
+
+//}
+
+
+
+//verilmis n m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+//int n = 1;
+//int m = 10;
+//int sum = 0;
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum = sum + i;
+
+//    }
+//}
+
+
+//Console.WriteLine(sum);
+
+
+
+
+//arrayin icindeki tek ededlerin cemini tapin
+
+//int[] numbers = { 1, 2, 4, 7, 6, 9, 12 };
+//int sum = 0;
+//foreach (var item in numbers)
+//{
+//    if (item % 2 == 1)
+//    {
+//        sum += item;
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+//Array in icindeki cut ededlerin sayi
+
+//int[] numbers = { 1, 2, 6, 7, 8, 11 };
+//int count = 0;
+//foreach (var item in numbers)
+//{
+//    if (item%2 == 0)
+//    {
+//        count += item;
+//    }
+//}
+
+//Console.WriteLine(count);
+
+
+
+//n ededinin 3 ve 7 ye qaliqsiz bolunub bolunmediyini tapin
+
+//int n = 21;
+
+//if ( n% 3 == 0 && n %7 == 0)
+//{
+//    Console.WriteLine("Qaliqsiz bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
